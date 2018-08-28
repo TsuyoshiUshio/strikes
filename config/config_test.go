2285ee9ac469aae3f3388bf639762b55f0acc8ce
Tsuyoshi Ushio <ushio@simplearchitect.com>
2018-08-28 16:05:07 +0900

    Adding unit test for config

--- config/config_test.go
+++ config/config_test.go
@@ -1,7 +1,36 @@
 package config
 
-import "testing"
+import (
+	"io/ioutil"
+	"os"
+	"os/user"
+	"testing"
 
-func TestGetConfigDir(t *testing.T) {
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetHomeDir(t *testing.T) {
+	homeDir, _ := getHomeDir()
+	usr, _ := user.Current()
+
+	assert.Equal(t, usr.HomeDir, homeDir)
+}
+
+func TestGetPowerPlantConfig(t *testing.T) {
+	content := `{"resourceGroup":"strikes-storage-japaneast","storageAccountName":"sastrikesdijjeqcx","storageAccountKey":"SomeKeys=="}`
+	file, err := ioutil.TempFile(".", "strikes")
+	defer os.Remove(file.Name())
+	if err != nil {
+		assert.Failf(t, "Can not open temp file", err.Error())
+		return
+	}
+	_, err = file.WriteString(content)
+	context := ConfigContext{
+		PowerPlantConfigFilePath: file.Name(),
+	}
+	config, err := context.GetPowerPlantConfig()
+	assert.Equal(t, "strikes-storage-japaneast", config.ResourceGroup)
+	assert.Equal(t, "sastrikesdijjeqcx", config.StorageAccountName)
+	assert.Equal(t, "SomeKeys==", config.StorageAccountKey)
 
 }
