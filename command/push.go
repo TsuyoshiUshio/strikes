236ce039fb032549579a9e0b573ae98f19c47b0f
Tsuyoshi Ushio <ushio@simplearchitect.com>
2018-09-06 17:16:09 +0900

    Implement zip/unzip

--- command/push.go
+++ command/push.go
@@ -1,16 +1,38 @@
 package command
 
-import "github.com/urfave/cli"
+import (
+	"log"
+	"path/filepath"
+
+	"github.com/TsuyoshiUshio/strikes/config"
+	"github.com/urfave/cli"
+)
 
 type PushCommand struct {
 }
 
 func (p *PushCommand) Push(c *cli.Context) error {
+
+	packageDirBase := c.String("p")
 	// Read the manifest file
-	// Read values file
+	packageDir := filepath.Join(packageDirBase, "circuit", "manifest.yaml")
+	// Read manifest file
+	manifest, err := config.NewManifestFromFile(packageDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = manifest.Validate()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	// create zip file for circuit
+
 	// upload both zips to blob storage
+
 	// rest api: create (or update) record
+
 	// successfully uploaded
+
 	return nil
 }
---
+++ helpers/zip_test.go
@@ -0,0 +1,35 @@
+package helpers
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/TsuyoshiUshio/strikes/helpers"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestZipScenario(t *testing.T) {
+	// Zip one directry recursively
+	// extract zip file in a temp folder
+	// check if there is a file on the deepest directry
+	tempPath := filepath.Join(".", ".test")
+
+	// If the tempPath exists, remove it
+	if _, err := os.Stat(tempPath); err == nil {
+		os.RemoveAll(tempPath)
+	}
+
+	os.MkdirAll(tempPath, os.ModePerm)
+	zipFilePath := filepath.Join(tempPath, "circuit.zip")
+	helpers.Zip(filepath.Join("..", "samples", "hello-world", "circuit"), zipFilePath)
+	helpers.UnZip(zipFilePath, tempPath)
+	manifestPath := filepath.Join(tempPath, "circuit", "manifest.yaml")
+	if _, err := os.Stat(manifestPath); os.IsNotExist(err) {
+		assert.Fail(t, "Unzipped file  manifest can not found at :"+manifestPath)
+	}
+
+	// clean up
+
+	os.RemoveAll(tempPath)
+}
