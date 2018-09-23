50dd8ba9f6eb8b3d6273f40e47f9541ff4207a19
Tsuyoshi Ushio <ushio@simplearchitect.com>
2018-09-23 10:45:21 +0800

    Adding SASQueryParameter

--- helpers/blob_test.go
+++ helpers/blob_test.go
@@ -17,3 +17,17 @@ func TestNewBlockBlob(t *testing.T) {
 	assert.Equal(t, ExpectedContainerName, blockBlob.ContainerName)
 	assert.Equal(t, ExpectedBlobName, blockBlob.BlobName)
 }
+
+func TestNewBlockBlobWithSASQueryParameter(t *testing.T) {
+	ExpectedBlockBlobURL := "https://foo.blob.core.windows.net/bar/baz?code=qux"
+	ExpectedStorageAccountName := "foo"
+	ExpectedContainerName := "bar"
+	ExpectedBlobName := "baz"
+	ExpectedSASQueryParameter := "code=qux"
+	blockBlob := NewBlockBlobWithSASQueryParameter(ExpectedStorageAccountName, ExpectedContainerName, ExpectedBlobName, ExpectedSASQueryParameter)
+	assert.Equal(t, ExpectedBlockBlobURL, blockBlob.BlockBlobURL.String())
+	assert.Equal(t, ExpectedStorageAccountName, blockBlob.StorageAccountName)
+	assert.Equal(t, ExpectedContainerName, blockBlob.ContainerName)
+	assert.Equal(t, ExpectedBlobName, blockBlob.BlobName)
+	assert.Equal(t, ExpectedSASQueryParameter, blockBlob.SASQueryParameter)
+}
