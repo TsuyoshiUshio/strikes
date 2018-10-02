4c4e784405d1551cc1f5f312afd5fcf3a1bf642c
Tsuyoshi Ushio <ushio@simplearchitect.com>
2018-10-02 14:31:54 +0900

    update sample to work for parameters

--- spikes/command/main.go
+++ spikes/command/main.go
@@ -9,17 +9,45 @@ import (
 	"os"
 	"os/exec"
 	"strconv"
+
+	"github.com/TsuyoshiUshio/strikes/config"
 )
 
 func main() {
-	command := os.Args[1]
+	var command string
+	if len(os.Args) == 1 {
+		command = "terrafrom"
+	} else {
+		command = os.Args[1]
+	}
 	// What do I want to achieve in this spike
 	// 1. How to check the external command
 	fmt.Println("Available: " + command + " " + strconv.FormatBool(isCommandAvailable(command)))
 	// 2. How to execute the extenarl command with flexible command
-	fmt.Println("start executing terraform init")
+	fmt.Println("start executing terraform plan")
+	context, err := config.NewConfigContext()
+	c, err := context.GetConfig()
+	if err != nil {
+		panic(err)
+	}
 	commandAndArgs := []string{
-		"init",
+		"plan",
+		"-var",
+		"environment_base_name=hello-world",
+		"-var",
+		"resource_group=hello-world-rg",
+		"-var",
+		"location=japaneast",
+		"-var", "package_name=hello-world",
+		"-var", "package_version=1.0.0",
+		"-var", "tag_name=hello-world",
+		"-var", "package_zip_name=hello-world.zip",
+		"-var", "language=dotnet",
+		"-var", "packages_sub_dir=hello-world/1.0.0/hello.zip",
+		"-var", fmt.Sprintf("client_id=%s", c.ClientID),
+		"-var", fmt.Sprintf("client_secret=%s", c.ClientSecret),
+		"-var", fmt.Sprintf("subscription_id=%s", c.SubscriptionID),
+		"-var", fmt.Sprintf("tenant_id=%s", c.TenantID),
 	}
 	cmd := exec.Command("terraform", commandAndArgs...) // ...enable us to pass them slice
 	stdout, err := cmd.StdoutPipe()                     // piping test
